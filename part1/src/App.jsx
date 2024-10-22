0a5cc90e4e41151d6f72f0cfa3a203c725d05b5d
Fil <filippo.piccardi@outlook.com>
2024-10-22 15:10:56 +0200

    exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,8 +6,18 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>
-      {props.part} {props.exercises}
+      {props.part} {props.exercise}
     </p>
   )
 }
@@ -22,17 +32,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
+
   const part1 = 'Fundamentals of React'
   const part2 = 'Using props to pass data'
   const part3 = 'State of a component'
+
+  const parts = [part1, part2, part3]
   const exercises = [10, 7, 14]
   
   return (
     <div>
       <Header title={course} />
-      <Content part={part1} exercises={exercises[0]}/>
-      <Content part={part2} exercises={exercises[1]}/>
-      <Content part={part3} exercises={exercises[2]}/>
+      <Content parts={parts} exercises={exercises}/>
       <Total exercises={exercises}/>
     </div>
   )
